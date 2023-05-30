577cd62f61d12b85ff537811dc337b65b2c52008
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-05-30 14:09:08 +0800

    открытие и закрытие попапов с формой

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -4,7 +4,12 @@ function ImagePopup() {
       <div className="popup__image container">
         <img className="popup__photo" src="#" />
         <h2 className="popup__caption" />
-        <button className="popup__close popup-img__close" type="button" aria-label="закрыть" />
+        <button 
+          className="popup__close popup-img__close" 
+          type="button" 
+          aria-label="закрыть" 
+          //onClick={}
+        />
       </div>
     </div>
   );
